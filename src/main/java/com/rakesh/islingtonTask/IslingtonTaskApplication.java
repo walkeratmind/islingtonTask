ca8f336f174022a7557bac479ceb5a9fcf7ea92b
Rakesh <rakeshhniraulaa@gmail.com>
2024-07-28 10:05:30 +0545

    initial commit

---
+++ src/main/java/com/rakesh/islingtonTask/IslingtonTaskApplication.java
@@ -0,0 +1,13 @@
+package com.rakesh.islingtonTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslingtonTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslingtonTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakesh/islingtonTask/IslingtonTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakesh.islingtonTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslingtonTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
